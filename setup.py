6598edbe6d1f277a5107c91e7faa22ac512bd2c0
v715 <vivekg@mit.edu>
2022-07-14 10:43:51 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Auto-differentiable digitally reconstructed radiographs in PyTorch.',
+    author='Vivek Gopalakrishnan',
+    license='MIT',
+)
