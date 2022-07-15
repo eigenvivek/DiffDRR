6904d2c4b39badb219f17f7f9c3d3ad1962a0c5a
v715 <vivekg@mit.edu>
2022-07-14 21:28:53 -0400

    Change origin -> source

--- src/models/siddon.py
+++ src/models/siddon.py
@@ -8,35 +8,35 @@ class Siddon:
         self.dims = torch.tensor(volume.shape) + 1.0
         self.volume = volume
 
-    def get_alpha(self, planes, origin, target):
-        return (self.center + planes * self.spacing - origin) / (target - origin)
+    def get_alpha(self, planes, source, target):
+        return (self.center + planes * self.spacing - source) / (target - source)
 
-    def get_alpha_minmax(self, origin, target):
+    def get_alpha_minmax(self, source, target):
         planes = torch.tensor([[1, 1, 1], self.dims]) - 1
-        alphas = self.get_alpha(planes, origin, target)
+        alphas = self.get_alpha(planes, source, target)
         minis = torch.min(alphas, dim=0).values
         maxis = torch.max(alphas, dim=0).values
         alphamin = torch.max(minis)
         alphamax = torch.min(maxis)
         return alphamin, alphamax, minis, maxis
 
-    def get_coords(self, alpha, origin, target):
-        pxyz = origin + alpha * (target - origin)
+    def get_coords(self, alpha, source, target):
+        pxyz = source + alpha * (target - source)
         return (pxyz - self.center) / self.spacing
 
-    def initialize(self, origin, target):
-        alphamin, alphamax, minis, maxis = self.get_alpha_minmax(origin, target)
-        idxmin = self.get_coords(alphamin, origin, target)
-        idxmax = self.get_coords(alphamax, origin, target)
+    def initialize(self, source, target):
+        alphamin, alphamax, minis, maxis = self.get_alpha_minmax(source, target)
+        idxmin = self.get_coords(alphamin, source, target)
+        idxmax = self.get_coords(alphamax, source, target)
 
-        # origin < target
+        # source < target
         # get minidx
         a = (alphamin == minis) * torch.ones(3)
         b = (alphamin != minis) * (idxmin + 1).trunc()
         # get maxidx
         c = (alphamax == maxis) * (self.dims - 1)
         d = (alphamax != maxis) * idxmax.trunc()
-        # origin > target
+        # source > target
         # get minidx
         e = (alphamax == maxis) * torch.ones(3)
         f = (alphamax != maxis) * (idxmax + 1).trunc()
@@ -44,33 +44,33 @@ class Siddon:
         g = (alphamin == minis) * (self.dims - 2)
         h = (alphamin != minis) * idxmin.trunc()
 
-        minidx = (origin < target) * (a + b) + (origin >= target) * (e + f)
-        maxidx = (origin < target) * (c + d) + (origin >= target) * (g + h)
+        minidx = (source < target) * (a + b) + (source >= target) * (e + f)
+        maxidx = (source < target) * (c + d) + (source >= target) * (g + h)
 
         return alphamin, alphamax, minidx, maxidx
 
-    def get_voxel_idx(self, alpha, origin, target):
-        idxs = self.get_coords(alpha, origin, target).trunc().int()
+    def get_voxel_idx(self, alpha, source, target):
+        idxs = self.get_coords(alpha, source, target).trunc().int()
         return list(idxs)
 
-    def raytrace(self, origin, target):
+    def raytrace(self, source, target):
 
         # Get the update conditions
         ones = torch.ones(3, dtype=int)
-        update_idxs = (origin < target) * ones - (origin >= target) * ones
-        update_alpha = self.spacing / torch.abs(target - origin)
+        update_idxs = (source < target) * ones - (source >= target) * ones
+        update_alpha = self.spacing / torch.abs(target - source)
 
         # Initialize the loop
-        alphamin, alphamax, minidx, maxidx = self.initialize(origin, target)
+        alphamin, alphamax, minidx, maxidx = self.initialize(source, target)
         alphacurr = alphamin
 
         # Get the potential next steps in the xyz planes
-        steps = self.get_alpha(minidx, origin, target)
+        steps = self.get_alpha(minidx, source, target)
         idx = steps.argmin()  # Find the smallest step
         alphanext = steps[idx]  # I.e., the next plane
 
         alphamid = (alphacurr + alphanext) / 2
-        voxel = self.get_voxel_idx(alphamid, origin, target)
+        voxel = self.get_voxel_idx(alphamid, source, target)
 
         step_length = alphanext - alphacurr
         d12 = step_length * self.volume[voxel]
