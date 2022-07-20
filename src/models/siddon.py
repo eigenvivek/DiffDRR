ed672011ca4c04c4c273895d50072307fe8ea81f
v715 <vivekg@mit.edu>
2022-07-20 13:22:41 -0400

    Add vectorized implementation

--- src/models/siddon.py
+++ src/models/siddon.py
@@ -2,7 +2,7 @@ import torch
 
 
 class Siddon:
-    def __init__(self, spacing, isocenter, volume, device=device):
+    def __init__(self, spacing, isocenter, volume, device):
         self.spacing = torch.tensor(spacing, device=device)
         self.isocenter = torch.tensor(isocenter, device=device)
         self.dims = torch.tensor(volume.shape, device=device) + 1.0
@@ -26,80 +26,94 @@ class Siddon:
         return alphamin, alphamax, minis, maxis
 
     def get_coords(self, alpha, source, target):
-        pxyz = source + alpha * (target - source)
+        pxyz = source + alpha.unsqueeze(-1) * (target - source)
         return (pxyz - self.isocenter) / self.spacing
 
     def initialize(self, source, target):
         alphamin, alphamax, minis, maxis = self.get_alpha_minmax(source, target)
-        alphamin = alphamin.expand(3, -1, -1).permute(1, 2, 0)
-        alphamax = alphamax.expand(3, -1, -1).permute(1, 2, 0)
+        alphamin_ = alphamin.unsqueeze(-1)
+        alphamax_ = alphamax.unsqueeze(-1)
+
         idxmin = self.get_coords(alphamin, source, target)
         idxmax = self.get_coords(alphamax, source, target)
 
         # source < target
         # get minidx
-        a = (alphamin == minis) * torch.ones(3, device=self.device)
-        b = (alphamin != minis) * (idxmin + 1).trunc()
+        a = (alphamin_ == minis) * torch.ones(3, device=self.device)
+        b = (alphamin_ != minis) * (idxmin + 1).trunc()
         # get maxidx
-        c = (alphamax == maxis) * (self.dims - 1)
-        d = (alphamax != maxis) * idxmax.trunc()
+        c = (alphamax_ == maxis) * (self.dims - 1)
+        d = (alphamax_ != maxis) * idxmax.trunc()
         # source > target
         # get minidx
-        e = (alphamax == maxis) * torch.ones(3, device=self.device)
-        f = (alphamax != maxis) * (idxmax + 1).trunc()
+        e = (alphamax_ == maxis) * torch.ones(3, device=self.device)
+        f = (alphamax_ != maxis) * (idxmax + 1).trunc()
         # get maxidx
-        g = (alphamin == minis) * (self.dims - 2)
-        h = (alphamin != minis) * idxmin.trunc()
+        g = (alphamin_ == minis) * (self.dims - 2)
+        h = (alphamin_ != minis) * idxmin.trunc()
 
         minidx = (source < target) * (a + b) + (source >= target) * (e + f)
         maxidx = (source < target) * (c + d) + (source >= target) * (g + h)
+        n_iters = (maxidx - minidx).max(dim=0).values.max(
+            dim=0
+        ).values.sum().int().item() + 1
 
-        return alphamin, alphamax, minidx, maxidx
+        return alphamin, alphamax, minidx, maxidx, n_iters
+
+    def get_next_step(self, steps):
+        alphanext, idxs = steps.min(dim=-1)
+        idxs = steps.argmin(dim=-1)
+        idxs = torch.dstack([idxs == 0, idxs == 1, idxs == 2])
+        return alphanext, idxs
 
     def get_voxel_idx(self, alpha, source, target):
-        idxs = self.get_coords(alpha.expand(3, -1, -1).permute(1, 2, 0), source, target)
-        idxs = idxs.trunc().long()
+        idxs = self.get_coords(alpha, source, target).trunc()
+        return idxs
+
+    def get_voxel(self, idxs):
         idxs = (
             idxs[:, :, 0]
             + idxs[:, :, 1] * self.volume.shape[1]
             + idxs[:, :, 2] * self.volume.shape[2]
         )
-        return idxs
+        return torch.take(self.volume, idxs.long())
 
-    def get_voxel(self, voxel_idxs):
-        return torch.take(self.volume, voxel_idxs)
+    def get_update(self, alphacurr, alphamax):
+        update_1 = alphacurr < alphamax
+        update_2 = torch.isclose(alphacurr, alphamax)
+        update_2 = torch.logical_not(update_2)
+        update = torch.logical_and(update_1, update_2)
+        return update.unsqueeze(-1)
 
     def raytrace(self, source, target):
 
         # Get the update conditions
-        ones = torch.ones(3, device=self.device, dtype=int)
+        ones = torch.ones(3, device=self.device)
         update_idxs = (source < target) * ones - (source >= target) * ones
         update_alpha = self.spacing / torch.abs(target - source)
 
         # Initialize the loop
-        alphamin, alphamax, minidx, maxidx = self.initialize(source, target)
-        alphacurr = alphamin[:, :, 0].clone()
+        alphamin, alphamax, minidx, _, n_iters = self.initialize(source, target)
+        alphamax = alphamax.clone()
+        alphacurr = alphamin.clone()
 
-        # Get the potential next steps in the xyz planes
+        # Get the potneital next steps in the xyz planes
         steps = self.get_alpha(minidx, source, target)
-        idx = steps.argmin(dim=-1)  # Find the smallest step
-        alphanext = steps[idx]  # I.e., the next plane
+        alphanext, idxs = self.get_next_step(steps)
 
-        alphamid = (alphacurr + alphanext) / 2
-        voxel = self.get_voxel_idx(alphamid, source, target)
+        alphamids = (alphacurr + alphanext) / 2
+        voxelidxs = self.get_voxel_idx(alphamids, source, target)
 
-        step_length = alphanext - alphacurr
-        d12 = step_length * self.volume[voxel]
+        drr = (alphanext - alphacurr) * self.get_voxel(voxelidxs)
         alphacurr = alphanext.clone()
 
         # Loop over all voxels that the ray passes through
-        while alphacurr < alphamax and not torch.isclose(alphacurr, alphamax):
-            voxel[idx] += update_idxs[idx]
-            steps[idx] += update_alpha[idx]
-            idx = steps.argmin()
-            alphanext = steps[idx]
-            step_length = alphanext - alphacurr
-            d12 += step_length * self.volume[voxel]
+        for _ in range(n_iters):
+            update = self.get_update(alphacurr, alphamax)
+            voxelidxs += update_idxs * idxs * update
+            steps += update_alpha * idxs * update
+            alphanext, idxs = self.get_next_step(steps)
+            drr += (alphanext - alphacurr) * self.get_voxel(voxelidxs)
             alphacurr = alphanext.clone()
 
-        return d12
+        return drr
