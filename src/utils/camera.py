afb39777e28bf22ec15929e91d7b36f3052f0c63
v715 <vivekg@mit.edu>
2022-07-26 13:23:56 -0400

    Reformat with black

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -4,7 +4,7 @@ import torch
 class Detector:
     """
     Construct a 6 DoF X-ray detector system. This model is based on a C-Arm.
-    
+
     Inputs
     ------
         sdr        : Source-to-Detector radius (half of the Source-to-Detector distance)
@@ -13,7 +13,7 @@ class Detector:
         gamma      : rotation of detector plane
         bx, by, bz : translation of the volume
     """
-    
+
     def __init__(self, sdr, theta, phi, gamma, bx, by, bz, device):
         self.sdr = torch.tensor([sdr], dtype=torch.float32, device=device)
         self.angles = torch.tensor([theta, phi, gamma], dtype=torch.float32, device=device, requires_grad=True)
@@ -35,21 +35,21 @@ class Detector:
         rays += center
         return source, rays
 
-    
+
 def _get_basis(rho, angles, device):
     # Get the rotation of 3D space
     R = rho * Rxyz(angles, device)
-    
+
     # Get the detector center and X-ray source
     source = R[:, 0]
     center = -source
-    
+
     # Get the basis of the detector plane (before translation)
     # TODO: normalizing the vectors seems to break the gradient, fix in future
     u, v = R[:, 1], R[:, 2]
     # u_ = u / torch.norm(u)
     # v_ = v / torch.norm(v)
-    
+
     return source, center, u, v
 
 
@@ -62,28 +62,52 @@ def Rxyz(angles, device):
 def Rx(theta, device):
     t0 = torch.zeros(1, device=device)[0]
     t1 = torch.ones(1, device=device)[0]
-    return torch.stack([
-        t1, t0              ,  t0,
-        t0, torch.cos(theta), -torch.sin(theta),
-        t0, torch.sin(theta),  torch.cos(theta)
-    ]).reshape(3, 3)
+    return torch.stack(
+        [
+            t1,
+            t0,
+            t0,
+            t0,
+            torch.cos(theta),
+            -torch.sin(theta),
+            t0,
+            torch.sin(theta),
+            torch.cos(theta),
+        ]
+    ).reshape(3, 3)
 
 
 def Ry(theta, device):
     t0 = torch.zeros(1, device=device)[0]
     t1 = torch.ones(1, device=device)[0]
-    return torch.stack([
-         torch.cos(theta), t0, torch.sin(theta),
-         t0              , t1, t0              ,
-        -torch.sin(theta), t0, torch.cos(theta)
-    ]).reshape(3, 3)
+    return torch.stack(
+        [
+            torch.cos(theta),
+            t0,
+            torch.sin(theta),
+            t0,
+            t1,
+            t0,
+            -torch.sin(theta),
+            t0,
+            torch.cos(theta),
+        ]
+    ).reshape(3, 3)
 
 
 def Rz(theta, device):
     t0 = torch.zeros(1, device=device)[0]
     t1 = torch.ones(1, device=device)[0]
-    return torch.stack([
-        torch.cos(theta), -torch.sin(theta), t0,
-        torch.sin(theta),  torch.cos(theta), t0,
-        t0              ,  t0              , t1
-    ]).reshape(3, 3)
+    return torch.stack(
+        [
+            torch.cos(theta),
+            -torch.sin(theta),
+            t0,
+            torch.sin(theta),
+            torch.cos(theta),
+            t0,
+            t0,
+            t0,
+            t1,
+        ]
+    ).reshape(3, 3)
