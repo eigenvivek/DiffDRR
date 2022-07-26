aec89467a8641d2c779c466812df0351ff59074f
v715 <vivekg@mit.edu>
2022-07-26 13:01:36 -0400

    Add device selector

--- src/__init__.py
+++ src/__init__.py
@@ -5,4 +5,5 @@ from .metrics.xcorr2 import xcorr2
 from .projectors.siddon import Siddon
 from .projectors.siddon_jacobs import SiddonJacobs
 
+from .utils.backend import get_device
 from .utils.camera import Detector
