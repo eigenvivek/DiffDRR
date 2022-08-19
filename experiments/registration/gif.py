41d34fc1376860331a6c65d418e65e860a06ca3c
v715 <vivekg@mit.edu>
2022-08-18 21:55:24 -0400

    Change savepaths

--- experiments/registration/gif.py
+++ experiments/registration/gif.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 
+import click
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from src import read_dicom, DRR
 from src.visualization import animate
 
 
-def make_dirs():
+def make_dirs(outdir):
     currdir = Path(__file__).parent
-    csvfiles = (currdir / "tmp").glob("*.csv")
-    converged = currdir / "gif/converged/"
-    not_converged = currdir / "gif/not_converged/"
+    csvfiles = (currdir / "results" / outdir).glob("*.csv")
+    converged = currdir / f"results/{outdir}/gif/converged/"
     converged.mkdir(parents=True, exist_ok=True)
+    not_converged = currdir / f"results/{outdir}/gif/not_converged/"
     not_converged.mkdir(parents=True, exist_ok=True)
     return csvfiles, converged, not_converged
 
@@ -26,17 +28,20 @@ def make_groundtruth():
     return drr, sdr, ground_truth
 
 
-def main():
-    csvfiles, converged, not_converged = make_dirs()
+@click.command()
+@click.option("--outdir", type=str, help="Directory with optimization runs.")
+def main(outdir):
+    csvfiles, converged, not_converged = make_dirs(outdir)
+    csvfiles = list(csvfiles)
     drr, sdr, ground_truth = make_groundtruth()
-    for csvfile in csvfiles:
+    for csvfile in tqdm(csvfiles):
         df = pd.read_csv(csvfile)
-        if len(df) < 500:
+        if df["loss"].iloc[-1] <= -0.999:
             outdir = converged
         else:
             outdir = not_converged
         outdir = outdir / f"{csvfile.stem}.gif"
-        animate(df[::10], sdr, drr, ground_truth, out=outdir)
+        animate(df[::10], sdr, drr, ground_truth, verbose=False, out=outdir)
 
 
 if __name__ == "__main__":
