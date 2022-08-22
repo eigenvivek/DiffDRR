26ce04193721cec02f77ec399a0f95539b68b121
v715 <vivekg@mit.edu>
2022-08-22 13:20:59 -0400

    Cap the number of gifs made

--- experiments/registration/gif.py
+++ experiments/registration/gif.py
@@ -29,17 +29,27 @@ def make_groundtruth():
 
 
 @click.command()
+@click.command("-n", "--n_gifs", type=int, default=10, help="Number of gifs to make.")
 @click.option("--outdir", type=str, help="Directory with optimization runs.")
-def main(outdir):
+def main(n_gifs, outdir):
     csvfiles, converged, not_converged = make_dirs(outdir)
     csvfiles = list(csvfiles)
     drr, sdr, ground_truth = make_groundtruth()
+
+    n_conv = 0
+    n_not_conv = 0
     for csvfile in tqdm(csvfiles):
         df = pd.read_csv(csvfile)
         if df["loss"].iloc[-1] <= -0.999:
+            if n_conv >= n_gifs:
+                continue
             outdir = converged
+            n_conv += 1
         else:
+            if n_not_conv >= n_gifs:
+                continue
             outdir = not_converged
+            n_not_conv += 1
         outdir = outdir / f"{csvfile.stem}.gif"
         animate(df, sdr, drr, ground_truth, verbose=False, out=outdir)
 
