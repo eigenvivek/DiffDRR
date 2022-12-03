630e4253fb71fba051dc0590c4dec9fccb0cdb08
eigenvivek <vivekg@mit.edu>
2022-12-03 16:28:57 -0500

    Fix dimensions in test

--- test/test_xray.py
+++ test/test_xray.py
@@ -2,14 +2,15 @@ import torch
 
 from diffdrr import Detector
 
+
 def test_Detector():
     try:
         detector = Detector(100, 100, 7.5e-2, 7.5e-2, device="cuda")
     except ValueError:
         detector = Detector(100, 100, 7.5e-2, 7.5e-2, device="cpu")
-    sdr = torch.tensor([200.0])
-    rotations = torch.rand(4, 3)
-    translations = torch.rand(4, 3)
+    sdr = torch.tensor([200.0]).to(detector.device)
+    rotations = torch.rand(4, 3).to(detector.device)
+    translations = torch.rand(4, 3).to(detector.device)
     source, target = detector.make_xrays(sdr, rotations, translations)
-    assert source.shape == (4, 3)
-    assert target.shape == (4, 100, 100, 3)
+    assert source.shape == (4, 1, 3)
+    assert target.shape == (4, 100 * 100, 3)
