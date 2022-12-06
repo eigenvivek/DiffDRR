5bd77b51e41eae25d91a1e2a55ff3670a60f1ede
v715 <vivekg@mit.edu>
2022-12-06 08:30:48 -0500

    Add project version

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import find_packages, setup
 
+
+# Get the version number from the version.py file
+with open("diffdrr/version.py") as f:
+    __version__ = f.read().split()[-1].strip("'")
+
 setup(
     name="diffdrr",
+    version=__version__,
     packages=find_packages(),
     version="0.1.0",
     description="Auto-differentiable digitally reconstructed radiographs in PyTorch.",
