9c7693b3c9041408978a69438a828d84e438ef71
v715 <vivekg@mit.edu>
2023-02-06 11:46:46 -0500

    Fix reshaping

--- diffdrr/utils/utils.py
+++ diffdrr/utils/utils.py
@@ -3,7 +3,7 @@ import torch
 
 def reshape_subsampled_drr(img, detector, batch_size):
     n_points = detector.height * detector.width
-    drr = torch.zeros(batch_size, n_points).to(detector.device)
+    drr = torch.zeros(batch_size, n_points).to(img)
     drr[:, detector.subsamples[-1]] = img
     drr = drr.view(batch_size, detector.height, detector.width)
     return drr
