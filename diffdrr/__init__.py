5fa983a655e47d58ef76fecab3c246a4964eab36
Vivek Gopalakrishnan <vivekg@mit.edu>
2023-02-23 15:51:09 -0500

    Initial commit with nbdev

--- diffdrr/__init__.py
+++ diffdrr/__init__.py
@@ -1,13 +1,1 @@
-from .drr import DRR
-
-from .data.load import load_example_ct, read_dicom
-
-from .metrics.xcorr2 import XCorr2
-from .metrics.ssd import SSD
-
-from .projectors.siddon import siddon_raycast
-
-from .utils.camera import Detector
-
-from .visualization.animation import animate
-from .visualization.visualize import plot_volume, plot_camera, plot_drr
+__version__ = "0.3.0"
--- diffdrr/visualization/__init__.py
+++
@@ -1,2 +0,0 @@
-from .animation import animate
-from .visualize import plot_volume, plot_camera, plot_drr
