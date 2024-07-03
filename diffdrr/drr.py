Fix gradients in Siddon's method (#296)
* Fix bugs in Siddon's method

* Remove warnings

* Revert default renderer to 'siddon'

* Rerun registration tutorial

diffdrr/drr.py
@@ -3,8 +3,6 @@
 # %% ../notebooks/api/00_drr.ipynb 3
 from __future__ import annotations
 
-import warnings
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -39,7 +37,7 @@ class DRR(nn.Module):
         reshape: bool = True,  # Return DRR with shape (b, 1, h, w)
         reverse_x_axis: bool = True,  # If True, obey radiologic convention (e.g., heart on right)
         patch_size: int | None = None,  # Render patches of the DRR in series
-        renderer: str = "trilinear",  # Rendering backend, either "siddon" or "trilinear"
+        renderer: str = "siddon",  # Rendering backend, either "siddon" or "trilinear"
         persistent: bool = True,  # Set persistent value in `torch.nn.Module.register_buffer`
         **renderer_kwargs,  # Kwargs for the renderer
     ):
@@ -98,9 +96,6 @@ class DRR(nn.Module):
         # Initialize the renderer
         if renderer == "siddon":
             self.renderer = Siddon(**renderer_kwargs)
-            warnings.warn(
-                "Gradients from Siddon's method are currently unstable for 2D/3D registration. Use 'trilinear' instead."
-            )
         elif renderer == "trilinear":
             self.renderer = Trilinear(**renderer_kwargs)
         else:

diffdrr/renderers.py
@@ -89,9 +89,9 @@ class Siddon(torch.nn.Module):
 def _get_alphas(source, target, dims, eps, filter_intersections_outside_volume):
     """Calculates the parametric intersections of each ray with the planes of the CT volume."""
     # Parameterize the parallel XYZ planes that comprise the CT volumes
-    alphax = torch.arange(dims[0] + 1).to(source) - 0.5
-    alphay = torch.arange(dims[1] + 1).to(source) - 0.5
-    alphaz = torch.arange(dims[2] + 1).to(source) - 0.5
+    alphax = torch.arange(dims[0] + 1).to(source)
+    alphay = torch.arange(dims[1] + 1).to(source)
+    alphaz = torch.arange(dims[2] + 1).to(source)
 
     # Calculate the parametric intersection of each ray with every plane
     sx, sy, sz = source[..., 0:1], source[..., 1:2], source[..., 2:3]
@@ -121,7 +121,7 @@ def _get_alpha_minmax(source, target, dims, eps):
     sdd = target - source + eps
 
     alpha0 = (torch.zeros(3).to(source) - source) / sdd
-    alpha1 = ((dims - 1).to(source) - source) / sdd
+    alpha1 = ((dims + 1).to(source) - source) / sdd
     alphas = torch.stack([alpha0, alpha1])
 
     alphamin = alphas.min(dim=0).values.max(dim=-1).values.unsqueeze(-1)